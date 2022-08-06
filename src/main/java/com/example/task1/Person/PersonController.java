774a028ad2465c332e7bb99b0d392dfa3c5c7d08
GucioWons <wgutek1@wp.pl>
2022-08-06 20:16:18 +0200

    Formatowanie moment

--- src/main/java/com/example/task1/Person/PersonController.java
+++ src/main/java/com/example/task1/Person/PersonController.java
@@ -1,20 +1,20 @@
 package com.example.task1.Person;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/generate")
 public class PersonController {
 
-    private PersonService personService;
+    private final PersonService personService;
 
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
     @GetMapping("/json/{quantity}")
-    public JsonNode personJsonEndpoint(@PathVariable int quantity) {return personService.personToJson(quantity);
+    public JsonNode personJsonEndpoint(@PathVariable int quantity) {
+        return personService.personToJson(quantity);
     }
 }
