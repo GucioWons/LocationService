f6d7a90cce523f3c866dfd733f4298343ed93a23
GucioWons <wgutek1@wp.pl>
2022-08-06 20:04:08 +0200

    Corrections

--- src/main/java/com/example/task1/Json/Json.java
+++ src/main/java/com/example/task1/Json/Json.java
@@ -2,14 +2,12 @@ package com.example.task1.Json;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class Json {
-    private final ObjectMapper objectMapper = getDefaultObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private ObjectMapper getDefaultObjectMapper(){
-        return new ObjectMapper();
-    }
     public JsonNode toJson(Object a){
         return objectMapper.valueToTree(a);
     }
--- src/main/java/com/example/task1/Person/PersonService.java
+++ src/main/java/com/example/task1/Person/PersonService.java
@@ -10,25 +10,35 @@ import java.util.Random;
 
 @Service
 public class PersonService {
+    private Json json;
 
-    static String[] names = new String[] {"Mróz", "Pawlak", "Marciniak","Przybylski","Szewczyk","Tomaszewski","Brzeziński",
+    private String[] names = new String[] {"Mróz", "Pawlak", "Marciniak","Przybylski","Szewczyk","Tomaszewski","Brzeziński",
             "Krawczyk","Kowalczyk","Mazur","Stępień","Baran","Krajewska","Kołodziej","Sawicki","Błaszczyk","Marciniak",
             "Wasilewska","Bąk","Górski",};
-    static Country[] countries= new Country[]{new Country("Poland", "PL", true),
+    private Country[] countries= new Country[]{new Country("Poland", "PL", true),
             new Country("Spain", "ES", true),
             new Country("United States of America", "US", false)};
-    static Random rand = new Random();
+    private Random rand = new Random();
+
+    public PersonService(Json json) {
+        this.json = json;
+    }
 
     public JsonNode personToJson(int quantity){
-        Json json = new Json();
         JsonNode[] jsons = new JsonNode[quantity];
         for(int i = 0; i<quantity; i++) {
-            int randomCountryNumber = rand.nextInt(3);
-            Person person = new Person("Position", names[rand.nextInt(20)], countries[randomCountryNumber].getName(),
-                    new Position(), countries[randomCountryNumber].isInEurope(), countries[randomCountryNumber].getCode(), rand.nextBoolean());
-            JsonNode personNode = json.toJson(person);
-            jsons[i] = personNode;
+            jsons[i] = generatePerson();
         }
         return json.toJson(jsons);
+    }
+
+    private JsonNode generatePerson(){
+        int randomCountryNumber = rand.nextInt(3);
+        Person person = new Person("Position",
+                names[rand.nextInt(20)], countries[randomCountryNumber].getName(),
+                new Position(), countries[randomCountryNumber].isInEurope(),
+                countries[randomCountryNumber].getCode(), rand.nextBoolean());
+        JsonNode personNode = json.toJson(person);
+        return personNode;
     }
 }
